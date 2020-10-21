59f161ef61ae7ab9c0c61082e4d2599732de5afa
Marcelo <60859440+Acharcitox@users.noreply.github.com>
2020-10-21 19:03:08 -0300

    Registro Usuario con Firebase, Pantalla Inicio, Enlaces a web externas

--- app/src/main/java/com/acharcitox/infoganaderia/IniciarSesionActivity.java
+++ app/src/main/java/com/acharcitox/infoganaderia/IniciarSesionActivity.java
@@ -1,14 +1,70 @@
 package com.acharcitox.infoganaderia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class IniciarSesionActivity extends AppCompatActivity {
+
+    private EditText correo;
+    private EditText contrasena;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iniciar_sesion);
+
+        correo = findViewById(R.id.correo);
+        contrasena = findViewById(R.id.contrasena);
+
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+
+    public void onStart(){
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    public void iniciarSesion(View view){
+
+        mAuth.signInWithEmailAndPassword(correo.getText().toString(), contrasena.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(getApplicationContext(), "Authentication succesfull.",
+                                    Toast.LENGTH_SHORT).show();
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(i);
+
+                        } else {
+
+                            //if sign fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+
+                    }
+                });
+
     }
 }
--- app/src/main/java/com/acharcitox/infoganaderia/MainActivity.java
+++ app/src/main/java/com/acharcitox/infoganaderia/MainActivity.java
@@ -3,6 +3,7 @@ package com.acharcitox.infoganaderia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 
@@ -20,7 +21,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void irRegistro(View view){
-        Intent i = new Intent(this, ResgistroActivity.class);
+        Intent i = new Intent(this, RegistroActivity.class);
+        startActivity(i);
+    }
+
+    public void irNoticias(View view){
+        Intent i = new Intent( Intent.ACTION_VIEW, Uri.parse("https://todosobreganado.com/category/noticias-sobre-ganaderia/"));
+        startActivity(i);
+    }
+
+    public void irConsejos(View view){
+        Intent i = new Intent( Intent.ACTION_VIEW, Uri.parse("https://ganaderia.win/category/consejos/"));
+        startActivity(i);
+    }
+
+    public void irRazas(View view){
+        Intent i = new Intent( Intent.ACTION_VIEW, Uri.parse("https://todosobreganado.com/razas/"));
+        startActivity(i);
+    }
+
+    public void irGanaderia(View view){
+        Intent i = new Intent( Intent.ACTION_VIEW, Uri.parse("https://todosobreganado.com/tipos-de-ganaderia/"));
         startActivity(i);
     }
 
